31d76d50c1311eaccd9d9b25926392e018023307
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-05-02 10:36:04 +0200

    changed constructor

--- src/main/java/pl/kurs/app/Runner.java
+++ src/main/java/pl/kurs/app/Runner.java
@@ -10,8 +10,8 @@ public class Runner {
     public static void main(String[] args) {
         Bike bike = new Bike("skladak1");
         Bike bike2 = new Bike("skladak2");
-        User user = new User("Seba");
-        User user2 = new User("Zbychu");
+        User user = new User("Seba", 20);
+        User user2 = new User("Zbychu", 40);
         BikeRentalService service = new BikeRentalService("Bikerrents");
 
         service.addBikeToRent(bike);
