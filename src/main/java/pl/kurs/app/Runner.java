f6f258127650cce1d03e079309399bfa6e21285b
Eryk Kowalczyk <kowalczykeryk@op.pl>
2025-05-02 08:52:20 +0200

    BikeRentalService final version

--- src/main/java/pl/kurs/app/Runner.java
+++ src/main/java/pl/kurs/app/Runner.java
@@ -1,7 +1,23 @@
 package pl.kurs.app;
 
+import pl.kurs.models.Bike;
+import pl.kurs.models.User;
+import pl.kurs.services.BikeRentalService;
+
+import java.time.LocalDate;
+
 public class Runner {
     public static void main(String[] args) {
-
+        Bike bike = new Bike("skladak1");
+        Bike bike2 = new Bike("skladak2");
+        User user = new User("Seba");
+        User user2 = new User("Zbychu");
+        BikeRentalService service = new BikeRentalService("Bikerrents");
+        service.addBikeToRent(bike);
+        service.addBikeToRent(bike2);
+        service.reserveBike(bike, LocalDate.of(2020, 5, 13), user);
+        service.reserveBike(bike, LocalDate.of(2020, 5, 14), user2);
+        service.cancelReservation(user2, bike2, LocalDate.of(2020, 5, 13));
+        service.showAvailableDays(bike);
     }
 }
--- src/main/java/pl/kurs/services/BikeRentalService.java
+++ src/main/java/pl/kurs/services/BikeRentalService.java
@@ -1,11 +1,88 @@
 package pl.kurs.services;
 
+import pl.kurs.models.Bike;
+import pl.kurs.models.User;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
 public class BikeRentalService {
     private String name;
+
+    private Map<Bike, Map<LocalDate, User>> availableBikes = new HashMap<>();
 
     public BikeRentalService(String name) {
         this.name = name;
     }
+
+    public void addBikeToRent(Bike bike){
+        availableBikes.put(bike, new HashMap<>());
+    }
+
+    public void reserveBike(Bike bike, LocalDate date, User user){
+        isBikeInOffer(bike);
+
+        if(isBikeReserved(bike, date)){
+            throw new IllegalArgumentException("W tym momencie rower nie jest dostępny");
+        }
+
+        availableBikes.get(bike).put(date, user);
+    }
+
+    public void cancelReservation(User user, Bike bike, LocalDate date){
+        isBikeInOffer(bike);  // sprawdzenie, czy rower jest w ofercie
+
+        if (!isBikeReserved(bike, date)) {
+            System.out.println("Tego dnia nie było żadnej rezerwacji dla tego roweru.");
+            return;
+        }
+
+        Map<LocalDate, User> reservations = availableBikes.get(bike);
+        User reservedUser = reservations.get(date);
+
+        if (!reservedUser.equals(user)) {
+            System.out.println("Tego dnia rower był zarezerwowany przez innego użytkownika.");
+            return;
+        }
+
+        // Jeśli wszystko się zgadza – usuwamy rezerwację
+        reservations.remove(date);
+        System.out.println("Rezerwacja anulowana.");
+
+
+    }
+
+    public void showAvailableDays(Bike bike){
+        System.out.println("Rower jest dostępny we wszystkich terminach oprócz: ");
+        isBikeInOffer(bike);
+        Map<LocalDate, User> reservations = availableBikes.get(bike);
+
+        if (reservations.isEmpty()) {
+            System.out.println("Brak rezerwacji – rower jest w pełni dostępny.");
+        } else {
+            for (LocalDate date : reservations.keySet()) {
+                System.out.println("Data: " + date);
+            }
+        }
+    }
+
+    private void isBikeInOffer(Bike bike){
+        if(!availableBikes.containsKey(bike)){
+            throw new IllegalArgumentException("Takiego roweru nie ma w ofercie");
+        }
+    }
+
+    private boolean isBikeReserved(Bike bike, LocalDate date){
+        if(availableBikes.containsKey(bike)){
+            Map<LocalDate, User> reservationsMap = availableBikes.get(bike);
+            if(reservationsMap.containsKey(date)){
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     @Override
     public String toString() {
